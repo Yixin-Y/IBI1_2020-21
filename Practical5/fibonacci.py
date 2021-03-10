a63298d728ca573faa8cfa3370e1833fb4fae21d
Yixin-Y <3200110888@zju.edu.cn>
2021-03-10 13:56:23 +0800

    fibonacci

---
+++ Practical5/fibonacci.py
@@ -0,0 +1,12 @@
+#Firstly, set up the first and second value
+#Secondly add n1 and n2 to create n3
+#Then give n1 the value of n2 and n2 the value of n3
+#Repeat the whole process and use i=i+1 to count
+n1=1
+n2=1
+for i in range of (3,14):
+	n3=n1+n2
+	n1=n2
+	n2=n3
+	i=i+1
+print n3
