6a52fae92f66a734cf3afd16d0f40676e7bcf854
Yixin-Y <3200110888@zju.edu.cn>
2021-03-10 14:20:30 +0800

    mystery code

---
+++ Practical5/mystery_code.py
@@ -0,0 +1,39 @@
+# What does this piece of code do?
+
+# Answer: randomly print an integer "n" which is bigger than 0 and smaller than 5
+
+
+
+# Import libraries
+
+# randint allows drawing a random number, 
+
+# e.g. randint(1,5) draws a number between 1 and 5from random import randint
+
+
+
+# ceil takes the ceiling of a number, i.e. the next higher integer.
+
+# e.g. ceil(4.2)=5
+
+from math import ceil
+
+
+
+
+
+p=False
+
+while p==False:
+	
+	p = True
+	
+	n = randint(1,100)
+	
+	if n > 50:
+		
+		p = False
+
+
+
+print(n)
